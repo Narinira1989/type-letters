25ee6d25a93a58ab9817c27bac5a427b0d03c779
Irina <irinanarin1989@gmail.com>
2023-07-21 20:15:57 +0300

    type-letters-project

---
+++ script.js
@@ -0,0 +1,14 @@
+let text = ('I am a web developer...');
+let i = 0;
+let speed = 100;
+
+function type(){
+    if(i < text.length){
+        document.querySelector('#par').textContent += text.charAt(i);
+        i++;
+    setTimeout(type, speed);
+    }
+}
+
+type();
+
